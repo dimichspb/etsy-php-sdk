7f74833f18834975e6649fdd86ae879131e5a939
Dmitry Tarantin <dimichspb@gmail.com>
2022-05-26 10:00:53 +0200

    Code clean uo

--- src/Resources/ShippingDestination.php
+++ src/Resources/ShippingDestination.php
@@ -9,6 +9,15 @@ use Etsy\Resource;
  *
  * @link https://developers.etsy.com/documentation/reference#operation/createShopShippingProfileDestination
  * @author Rhys Hall hello@rhyshall.com
+ *
+ * @property float $shipping_profile_destination_id
+ * @property float $shipping_profile_id
+ * @property float $shop_id
+ * @property string $destination_country_iso
+ * @property int $min_delivery_days
+ * @property int $max_delivery_days
+ * @property float $shipping_carrier_id
+ * @property string $mail_class
  */
 class ShippingDestination extends Resource {
 
@@ -17,9 +26,10 @@ class ShippingDestination extends Resource {
    *
    * @link https://developers.etsy.com/documentation/reference#operation/updateShopShippingProfileDestination
    * @param array $data
-   * @return Etsy\Resources\ShippingDestination
+   * @return ShippingDestination
    */
-  public function update(array $data) {
+  public function update(array $data): ShippingDestination
+  {
     return $this->updateRequest(
       "/application/shops/{$this->shop_id}/shipping-profiles/{$this->shipping_profile_id}/destinations/{$this->shipping_profile_destination_id}",
       $data
@@ -32,7 +42,8 @@ class ShippingDestination extends Resource {
    * @link https://developers.etsy.com/documentation/reference#operation/deleteShopShippingProfileDestination
    * @return boolean
    */
-  public function delete() {
+  public function delete(): bool
+  {
     return $this->deleteRequest(
       "/application/shops/{$this->shop_id}/shipping-profiles/{$this->shipping_profile_id}/destinations/{$this->shipping_profile_destination_id}"
     );
