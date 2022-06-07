62a4ddcd66300fbb4bf1e1525178b881fb3932cd
Dmitry Tarantin <dimichspb@gmail.com>
2022-06-07 13:03:28 +0200

    Updating ListingImage propeties

--- src/Resources/ListingImage.php
+++ src/Resources/ListingImage.php
@@ -9,6 +9,11 @@ use Etsy\Resource;
  *
  * @link https://developers.etsy.com/documentation/reference#tag/ShopListing-Image
  * @author Rhys Hall hello@rhyshall.com
+ *
+ * @property string $url_fullxfull
+ * @property string $listing_image_id
+ * @property string $listing_id
+ * @property string $shop_id
  */
 class ListingImage extends Resource {
 
@@ -18,7 +23,8 @@ class ListingImage extends Resource {
    * @link https://developers.etsy.com/documentation/reference#operation/deleteListingImage
    * @return boolean
    */
-  public function delete() {
+  public function delete(): bool
+  {
     return $this->deleteRequest(
       "/application/shops/{$this->shop_id}/listings/{$this->listing_id}/images/{$this->listing_image_id}"
     );
